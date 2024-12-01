0ed946e84e6bc0cc860b37999810b6fc98bcc5a2
Jhon-Crow <jhamish@mail.ru>
2024-12-01 18:59:08 +0300

    add autoscroll to HistoryList

--- src/widgets/HistoryList/HistoryList.tsx
+++ src/widgets/HistoryList/HistoryList.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
@@ -6,32 +7,43 @@ import O from "../../shared/assets/icons/components/o.tsx";
 import X from "../../shared/assets/icons/components/x.tsx";
 import { dataItem } from "../../pages/Main.tsx";
 
-
 interface HistoryListProps {
     state?: dataItem[];
 }
 
-const HistoryList = ({state}: HistoryListProps) => {
+const HistoryList = ({ state }: HistoryListProps) => {
+    const listRef = useRef<HTMLUListElement | null>(null);
+
+    useEffect(() => {
+        if (listRef.current) {
+            listRef.current.scrollTop = -listRef.current.scrollHeight;
+        }
+    }, [state]);
 
     return (
-        <List sx={{
-            width: '100%',
-            bgcolor: 'lightgray',
-            maxHeight: '280px',
-            overflowY: 'auto',
-            display: 'flex',
-            flexDirection: 'column-reverse'
-        }}>
-            {state && state.map(({date, cross, circle}, index) => {
-                    return (<ListItem key={date + index} sx={{border: '1px solid grey'}}>
-                        <ListItemAvatar>
-                            {cross && <X height={20} width={20}/>}
-                            {circle && <O height={20} width={20}/>}
-                        </ListItemAvatar>
-                        <ListItemText primary={!cross && !circle && 'Ничья!' || cross ? 'Крестики победили!' : 'Нолики победили!'} secondary={date}/>
-                    </ListItem>)
-                }
-            )}
+        <List
+            ref={listRef}
+            sx={{
+                width: '100%',
+                bgcolor: 'lightgray',
+                maxHeight: '280px',
+                overflowY: 'auto',
+                display: 'flex',
+                flexDirection: 'column-reverse',
+            }}
+        >
+            {state && state.map(({ date, cross, circle }, index) => (
+                <ListItem key={date + index} sx={{ border: '1px solid grey' }}>
+                    <ListItemAvatar>
+                        {cross && <X height={20} width={20} />}
+                        {circle && <O height={20} width={20} />}
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary={!cross && !circle ? 'Ничья!' : cross ? 'Крестики победили!' : 'Нолики победили!'}
+                        secondary={date}
+                    />
+                </ListItem>
+            ))}
         </List>
     );
 };
